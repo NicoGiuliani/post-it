8cd929ee9c5cc417b81bbb9e883d8d7f9dff6b6a
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-04-14 18:00:40 -0700

    Got posts saving to the db

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -1,13 +1,34 @@
 "use client"
 
 import { useState } from "react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from "axios"
 
 export default function CreatePost() {
   const [title, setTitle] = useState("")
   const [isDisabled, setIsDisabled] = useState(false)
 
+  const { mutate } = useMutation(
+    async (title: string) => await axios.post("/api/posts/addPost", { title }),
+    {
+      onError: (error) => {
+        console.log(error)
+      },
+      onSuccess: (data) => {
+        setTitle('')
+        setIsDisabled(false)
+        console.log(data)
+      }
+    })
+
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setIsDisabled(true)
+    mutate(title)
+  }
+
   return (
-    <form className="bg-white my-8 p-8 rounded-md">
+    <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
       <div className="flex flex-col my-4">
         <textarea
           onChange={(e) => setTitle(e.target.value)}
@@ -17,7 +38,7 @@ export default function CreatePost() {
           value={title}></textarea>
       </div>
       <div className="flex items-center justify-between gap-2">
-        <p className="font-bold text-sm">{`${title.length}/300`}</p>
+        <p className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray-700"}`}>{`${title.length}/300`}</p>
         <button
           disabled={isDisabled}
           className="text-sm bg-teal-600 text-white py-2 px-6 rounded-md disabled:opacity-25"
